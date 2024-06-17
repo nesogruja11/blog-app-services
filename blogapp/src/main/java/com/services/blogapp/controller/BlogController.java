8de6222c8ea6233c4d36065edaf8c8b8992a7c1d
grujicicn <nesogruja11@gmail.com>
2024-06-17 14:45:05 +0200

    Napravljene su sitne izmjene.

--- blogapp/src/main/java/com/services/blogapp/controller/BlogController.java
+++ blogapp/src/main/java/com/services/blogapp/controller/BlogController.java
@@ -1,6 +1,5 @@
 package com.services.blogapp.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +7,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/blog")
 public class BlogController {
 
-	@Autowired
-	BlogService blogService;
 }
--- blogapp/src/main/java/com/services/blogapp/service/BlogService.java
+++ blogapp/src/main/java/com/services/blogapp/service/BlogService.java
@@ -3,7 +3,6 @@ package com.services.blogapp.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.services.blogapp.model.Blog;
 import com.services.blogapp.repository.BlogRepository;
 
 @Service
@@ -12,7 +11,4 @@ public class BlogService {
 	@Autowired
 	BlogRepository blogRepository;
 
-	public Blog save(BlogSaveDto blogSaveDto) {
-		return null;
-	}
 }
