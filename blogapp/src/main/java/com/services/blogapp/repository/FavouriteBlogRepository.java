b4873cec44a9940ca712d11836a62821d41c0e7c
bilinacd <bilinacd99@gmail.com>
2024-06-27 13:32:03 +0200

    Dodat je servis za brisanje omiljenih blogova

--- blogapp/src/main/java/com/services/blogapp/repository/FavouriteBlogRepository.java
+++ blogapp/src/main/java/com/services/blogapp/repository/FavouriteBlogRepository.java
@@ -3,10 +3,13 @@ package com.services.blogapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.services.blogapp.model.Blog;
 import com.services.blogapp.model.FavouriteBlog;
 import com.services.blogapp.model.FavouriteBlogKey;
+import com.services.blogapp.model.User;
 
 @Repository
 public interface FavouriteBlogRepository extends JpaRepository<FavouriteBlog, FavouriteBlogKey> {
+	int deleteByUserAndBlog(User user, Blog blog);
 
 }
