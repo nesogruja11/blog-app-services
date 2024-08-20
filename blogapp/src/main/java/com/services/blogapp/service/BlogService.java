6984b61ec4981914a2db6e5b16529d5d0ee596f1
bilinacd <bilinacd99@gmail.com>
2024-08-20 13:55:02 +0200

    Odobreni blogovi sortirani po blogId-u

--- blogapp/src/main/java/com/services/blogapp/service/BlogService.java
+++ blogapp/src/main/java/com/services/blogapp/service/BlogService.java
@@ -1,9 +1,11 @@
 package com.services.blogapp.service;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -83,7 +85,9 @@ public class BlogService {
 	}
 
 	public List<Blog> getApprovedBlogs() {
-		return blogRepository.findByApprovedTrue();
+		List<Blog> approvedBlogs = blogRepository.findByApprovedTrue();
+		return approvedBlogs.stream().sorted(Comparator.comparing(Blog::getBlogId).reversed())
+				.collect(Collectors.toList());
 	}
 
 	@Transactional
